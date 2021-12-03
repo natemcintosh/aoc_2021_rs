980e0a4604b920638537b19a942e62e0758ff569
Nate McIntosh <40805156+natemcintosh@users.noreply.github.com>
2021-12-03 13:22:55 -0500

    updates to day 3

--- examples/day03.rs
+++ examples/day03.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, hash::Hash};
+
 fn parse_input(input: &str) -> Vec<Vec<char>> {
     // For each row and column, we store the character at that position.
     input.lines().map(|line| line.chars().collect()).collect()
@@ -43,7 +45,7 @@ fn part1(input: &[Vec<char>]) -> usize {
     let gamma = usize::from_str_radix(
         &most_common_char_in_each_col
             .iter()
-            .map(|c| c.to_string())
+            .map(std::string::ToString::to_string)
             .collect::<String>(),
         2,
     )
@@ -56,13 +58,55 @@ fn part1(input: &[Vec<char>]) -> usize {
     let epsilon = usize::from_str_radix(
         &least_common_char_in_each_col
             .iter()
-            .map(|c| c.to_string())
+            .map(std::string::ToString::to_string)
             .collect::<String>(),
         2,
     )
     .expect("Could not parse epsilon");
 
     gamma * epsilon
+}
+
+fn bit_criteria(input: &[char], is_oxygen: bool) -> char {
+    let mut counts: [u32; 2] = [0, 0];
+    for c in input {
+        if *c == '0' {
+            counts[0] += 1;
+        } else {
+            counts[1] += 1;
+        }
+    }
+
+    match (counts[0], counts[1], is_oxygen) {
+        (n_zeros, n_ones, oxy) if (n_zeros > n_ones) && oxy => '0',
+        (n_zeros, n_ones, oxy) if (n_zeros > n_ones) && !oxy => '1',
+        (n_zeros, n_ones, oxy) if (n_zeros < n_ones) && oxy => '1',
+        (n_zeros, n_ones, oxy) if (n_zeros < n_ones) && !oxy => '0',
+        (_, _, oxy) if oxy => '1',
+        (_, _, oxy) if !oxy => '0',
+        _ => unreachable!(),
+    }
+}
+
+fn get_rows<T: Clone>(v: &[Vec<T>], rows: HashSet<usize>) -> Vec<Vec<T>> {
+    // Return the rows in the set
+    v.iter()
+        .enumerate()
+        .filter(|(idx, _)| rows.contains(&idx))
+        .map(|(i, _)| v[i].clone())
+        .collect()
+}
+
+fn get_cols<T: Clone>(v: &[Vec<T>], cols: HashSet<usize>) -> Vec<Vec<T>> {
+    // Return only the columns of `v` that are in the set `cols`
+    let mut result = vec![Vec::new(); cols.len()];
+    for (row_idx, row) in v.iter().enumerate() {
+        for (col_idx, &item) in row.iter().enumerate() {
+            if cols.contains(&col_idx) {
+                result[row_idx].push(item.clone());
+            }
+        }            
+    return result;
 }
 
 fn part2(input: &[Vec<char>]) -> usize {
@@ -139,6 +183,70 @@ fn test_part1() {
     ];
 
     assert_eq!(part1(&input), 198);
+}
+
+#[test]
+fn test_get_rows() {
+    let input: Vec<Vec<char>> = vec![
+        vec!['0', '0', '1', '0', '0'],
+        vec!['1', '1', '1', '1', '0'],
+        vec!['1', '0', '1', '1', '0'],
+        vec!['1', '0', '1', '1', '1'],
+        vec!['1', '0', '1', '0', '1'],
+        vec!['0', '1', '1', '1', '1'],
+        vec!['0', '0', '1', '1', '1'],
+        vec!['1', '1', '1', '0', '0'],
+        vec!['1', '0', '0', '0', '0'],
+        vec!['1', '1', '0', '0', '1'],
+        vec!['0', '0', '0', '1', '0'],
+        vec!['0', '1', '0', '1', '0'],
+    ];
+
+    let rows: HashSet<usize> = vec![0, 3, 8].into_iter().collect();
+    let expected: Vec<Vec<char>> = vec![
+        vec!['0', '0', '1', '0', '0'],
+        vec!['1', '0', '1', '1', '1'],
+        vec!['1', '0', '0', '0', '0'],
+    ];
+    let got = get_rows(&input, rows);
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn test_get_cols() {
+    let input: Vec<Vec<char>> = vec![
+        vec!['0', '0', '1', '0', '0'],
+        vec!['1', '1', '1', '1', '0'],
+        vec!['1', '0', '1', '1', '0'],
+        vec!['1', '0', '1', '1', '1'],
+        vec!['1', '0', '1', '0', '1'],
+        vec!['0', '1', '1', '1', '1'],
+        vec!['0', '0', '1', '1', '1'],
+        vec!['1', '1', '1', '0', '0'],
+        vec!['1', '0', '0', '0', '0'],
+        vec!['1', '1', '0', '0', '1'],
+        vec!['0', '0', '0', '1', '0'],
+        vec!['0', '1', '0', '1', '0'],
+    ];
+
+    let cols: HashSet<usize> = vec![0, 2, 4].into_iter().collect();
+
+    let expected: Vec<Vec<char>> = vec![
+        vec!['0', '1', '0'],
+        vec!['1', '1', '0'],
+        vec!['1', '1', '0'],
+        vec!['1', '1', '1'],
+        vec!['1', '1', '1'],
+        vec!['0', '1', '1'],
+        vec!['0', '1', '1'],
+        vec!['1', '1', '0'],
+        vec!['1', '0', '0'],
+        vec!['1', '0', '1'],
+        vec!['0', '0', '0'],
+        vec!['0', '0', '0'],
+    ];
+    let got = get_cols(&input, cols);
+    assert_eq!(got, expected);
 }
 
 #[test]
